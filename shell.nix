0831b251efcbed8d307db1e3d2e1e20aa94de796
marcusnewton <marcus.newton@hotmail.com>
2021-09-14 18:30:28 +1000

    add npm publish scripts

--- shell.nix
+++ shell.nix
@@ -1,10 +1,25 @@
 let
  pkgs = import <nixpkgs> {};
+
+ prepack = pkgs.writeShellScriptBin "prepack" ''
+ npm run compile
+
+ rm -rf artifacts
+ mkdir -p artifacts
+ cp build/contracts/*.json artifacts
+ '';
+
+ publish = pkgs.writeShellScriptBin "publish" ''
+ npm pack
+ npm publish ./configurable-rights-pool-0.0.1.tgz --access public
+ '';
 in
 pkgs.stdenv.mkDerivation {
  name = "shell";
  buildInputs = [
   pkgs.nodejs-14_x
+  prepack
+  publish
  ];
 
  shellHook = ''
