f46eff226611be43b5c1c30084247f0f99dc69d1
marcusnewton <marcus.newton@hotmail.com>
2021-09-21 17:02:54 +1000

    add npm publish CI workflow

--- shell.nix
+++ shell.nix
@@ -1,30 +1,25 @@
 let
- pkgs = import <nixpkgs> {};
+  pkgs = import <nixpkgs> { };
 
- prepack = pkgs.writeShellScriptBin "prepack" ''
- npm run compile
+  prepack = pkgs.writeShellScriptBin "prepack" ''
+    npm run compile
 
- rm -rf artifacts
- mkdir -p artifacts
- cp build/contracts/*.json artifacts
- '';
-
- publish = pkgs.writeShellScriptBin "publish" ''
- npm pack
- npm publish ./configurable-rights-pool-0.0.2.tgz --access public
- '';
+    rm -rf artifacts
+    mkdir -p artifacts
+    cp build/contracts/*.json artifacts
+  '';
 in
 pkgs.stdenv.mkDerivation {
- name = "shell";
- buildInputs = [
-  pkgs.nodejs-14_x
-  prepack
-  publish
- ];
+  name = "shell";
+  buildInputs = [
+    pkgs.nixpkgs-fmt
+    pkgs.nodejs-14_x
+    prepack
+  ];
 
- shellHook = ''
-  export PATH=$( npm bin ):$PATH
-  # keep it fresh
-  npm install
- '';
+  shellHook = ''
+    export PATH=$( npm bin ):$PATH
+    # keep it fresh
+    npm install
+  '';
 }
